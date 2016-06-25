292b46272f4803037d5de69aae50801abbf3cb05
Matt Robinson <brimstone@the.narro.ws>
2016-06-25 15:39:16 -0400

    Adding comments

--- main.go
+++ main.go
@@ -8,18 +8,25 @@ import (
 )
 
 func main() {
+
+	// Create client using environment variables, if present
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
 	}
 
+	// Setup default catch-all handler
 	http.Handle("/", &dcgi.Handler{
 		Image:  "root",
 		Client: cli,
 	})
+
+	// Setup specific asset handler, trailing slash is important
 	http.Handle("/assets/", &dcgi.Handler{
 		Image:  "assets",
 		Client: cli,
 	})
+
+	// Start listener
 	http.ListenAndServe(":8080", nil)
 }
